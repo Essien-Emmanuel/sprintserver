8731fc3b82fd7af58e5562fe5486603f9a18db38
Essien Emmanuel <essienemma300@gmail.com>
2024-12-02 14:52:08 +0100

    wrapup #1

--- src/index.js
+++ src/index.js
@@ -155,36 +155,40 @@ class SprintServer {
 }
 
 /** usage */
-const app = new SprintServer();
-const router = app.Router();
+// const app = new SprintServer();
+// const router = app.Router();
 
-router.get(
-  "/",
-  (req, resp, proceed) => {
-    req.new = "setting this newly";
-    console.log("middleware...");
-  },
-  (req, resp) => {
-    resp.send("getting /");
-  }
-);
+// router.get(
+//   "/",
+//   (req, res, proceed) => {
+//     console.log('testing');
+//     proceed()
+//   },
+//   (req, resp, proceed) => {
+//     req.new = "setting this newly";
+//     console.log("middleware...");
+//   },
+//   (req, resp) => {
+//     resp.send("getting /");
+//   }
+// );
 
-router.get("/home", (req, resp) => {
-  resp.json("getting /home");
-});
+// router.get("/home", (req, resp) => {
+//   resp.json("getting /home");
+// });
 
-router.post("/", (req, resp, proceed) => {
-  const data = req.body;
+// router.post("/", (req, resp, proceed) => {
+//   const data = req.body;
 
-  resp.json({
-    data: {
-      name: data.name,
-      email: data.email,
-      status: "new user",
-      date: Date.now(),
-    },
-  });
-});
+//   resp.json({
+//     data: {
+//       name: data.name,
+//       email: data.email,
+//       status: "new user",
+//       date: Date.now(),
+//     },
+//   });
+// });
 
 // app.listen(1234, () => {
 //   console.log("server spinning at port 1234");
