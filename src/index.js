feb4423dab15465f8bc4d9864a1f6ccd1e29e740
Essien Emmanuel <essienemma300@gmail.com>
2024-12-02 14:56:24 +0100

    clean up

--- src/index.js
+++ src/index.js
@@ -107,9 +107,6 @@ class SprintServer {
         throw new Error("Not found");
       }
 
-      // use middlewares
-
-      //
       const reqBody = await bodyParser(req);
       req.body = reqBody;
 
@@ -153,45 +150,5 @@ class SprintServer {
     server.listen(port, cb());
   }
 }
-
-/** usage */
-// const app = new SprintServer();
-// const router = app.Router();
-
-// router.get(
-//   "/",
-//   (req, res, proceed) => {
-//     console.log('testing');
-//     proceed()
-//   },
-//   (req, resp, proceed) => {
-//     req.new = "setting this newly";
-//     console.log("middleware...");
-//   },
-//   (req, resp) => {
-//     resp.send("getting /");
-//   }
-// );
-
-// router.get("/home", (req, resp) => {
-//   resp.json("getting /home");
-// });
-
-// router.post("/", (req, resp, proceed) => {
-//   const data = req.body;
-
-//   resp.json({
-//     data: {
-//       name: data.name,
-//       email: data.email,
-//       status: "new user",
-//       date: Date.now(),
-//     },
-//   });
-// });
-
-// app.listen(1234, () => {
-//   console.log("server spinning at port 1234");
-// });
 
 export default SprintServer;
